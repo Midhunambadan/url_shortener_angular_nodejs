6e4d7e24ba39e9e67b9464fdefb873806b7718df
Midhun Ambadan <midhunambadan06@gmail.com>
2025-04-02 20:03:24 +0530

    mongodb connected

---
+++ backend_url_shortener/models/userModel.js
@@ -0,0 +1,26 @@
+const mongoose=require('mongoose')
+
+const userModel=new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique: true
+        },
+        password:{
+            type:String,
+            required:true
+        },
+        url:{
+            type:String,
+        }
+    }
+)
+
+const User=mongoose.model('User',userModel)
+
+module.exports=User
--- backend_url_shortener/server.js
+++ backend_url_shortener/server.js
@@ -1,17 +1,35 @@
 
 const express=require('express')
 const app=express()
+const mongoose=require('mongoose')
+
 require('dotenv').config()
 const cors=require('cors')
 const PORT = process.env.PORT || 5000
 
 app.use(cors())
+app.use(express.json())
+
+
+
+mongoose.connect(process.env.MONGO_URI,{
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+})
+.then(()=>{
+    console.log(' MongoDB Connected')
+
+}).catch((err)=>{
+    console.error('MongoDB Connection Error:', err)
+}
+    
+)
 
 app.get('/',(req,res)=>{
     res.send('Hellooo')
 })
 
 app.listen(PORT,()=>{
-    console.log(`Server is runng at http://localhost:${PORT}`);
+    console.log(`Server is runng at http://localhost:${PORT}`)
     
 })
