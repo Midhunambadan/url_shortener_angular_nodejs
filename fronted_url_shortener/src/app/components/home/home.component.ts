123908ef2abb03e104601faffd6e9224631aa2ac
Midhun Ambadan <midhunambadan06@gmail.com>
2025-04-03 00:42:07 +0530

    cards added

--- fronted_url_shortener/src/app/components/home/home.component.ts
+++ fronted_url_shortener/src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
+import { CardsComponent } from '../cards/cards.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,SearchBarComponent],
+  imports: [HeaderComponent,SearchBarComponent,CardsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
